8c2923de8086949eea90f1f6abdf337edb9d3cb1
MJ329 <durowadeabdulmajeed@gmail.com>
2020-07-02 02:18:45 -0600

    Updates

---
+++ precache-manifest.4b524215fd55727782a1a9015833ffde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e75f6e91acc85856bb5da4278a75a8",
+    "url": "/todolist-in-react/index.html"
+  },
+  {
+    "revision": "3dd7c8b08a94e937c62e",
+    "url": "/todolist-in-react/static/css/main.4ebe9f6f.chunk.css"
+  },
+  {
+    "revision": "22d17d1a0e365fa09123",
+    "url": "/todolist-in-react/static/js/2.fe68eac3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist-in-react/static/js/2.fe68eac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dd7c8b08a94e937c62e",
+    "url": "/todolist-in-react/static/js/main.1cb35b86.chunk.js"
+  },
+  {
+    "revision": "b8fd7b1edcfb7c2a2911",
+    "url": "/todolist-in-react/static/js/runtime-main.e994d874.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todolist-in-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
